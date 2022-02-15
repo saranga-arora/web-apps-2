b867f774e0696f598139edf9ab876fba85f623fd
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-02-15 17:30:23 +0000

    adds contacts code

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,16 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params["id"])
   end
+  
+  def new 
+    @contact = Contact.new
+    @contact.company_id = params["company_id"] #to transfer state of company
+  end 
+
+  def create
+    @contact = Contact.new(params["contact"])
+    @contact.save
+    redirect_to "/companies"
+  end 
 
 end
