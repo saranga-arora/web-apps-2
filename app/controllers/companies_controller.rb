aef99104ff2fd7fa4080e73302312995442a9cc3
saranga-arora <saranga.arora@kellogg.northwestern.edu>
2022-02-15 16:35:52 +0000

    adds companies code

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,5 +8,18 @@ class CompaniesController < ApplicationController
     # .find(123) is the same as .where({ id: 123 })[0]
     @company = Company.find(params["id"])
   end
+  
+  def new 
+    @company = Company.new
+  end 
+
+  def create
+    @company = Company.new(params["company"]) #create new company based on params from user
+    @company.save #save info
+    redirect_to "/companies" #send user back to list of companies
+  end 
 
 end
+
+
+
